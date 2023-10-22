53774ecf30b73931da5deca7027c4da138663bae
seprov <95832216+seprov@users.noreply.github.com>
2023-10-22 12:55:25 -0500

    first commit

---
+++ src/NutritionDataService/Host.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NutritionDataService
+{
+    internal class Host
+    {
+        public IServiceProvider ServiceProvider => new HostBuilder()
+            .ConfigureAppConfiguration(ConfigureAppConfiguration)
+            .ConfigureServices(ConfigureServices)
+            .Build().Services;
+
+        private void ConfigureAppConfiguration(HostBuilderContext context, IConfigurationBuilder configuration)
+        {
+            throw new NotImplementedException();
+        }
+        private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services
+                .AddSingleton<UsdaClient>()
+            ;
+        }
+
+    }
+}
---
+++ src/NutritionDataService/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NutritionDataService;
+
+
+Console.WriteLine("Hello, World!");
+
+var host = new Host();
