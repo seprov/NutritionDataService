0a1cb3964979339916767e8cfe743dbacead8df5
seprov <95832216+seprov@users.noreply.github.com>
2023-10-24 08:51:02 -0500

    begin adding web support

--- src/NutritionDataService/Program.cs
+++ src/NutritionDataService/Program.cs
@@ -1,11 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.Extensions.DependencyInjection;
-using NutritionDataService;
-
 
 Console.WriteLine("Hello, World!");
 
-var serviceProvider = new Host().ServiceProvider;
+var serviceProvider = new NutritionDataService.Host().ServiceProvider;
 
 var usdaClient = serviceProvider.GetRequiredService<NutritionDataService.Usda.Client>();
 var x = await usdaClient.GetNutritionDataAsync("milk");
